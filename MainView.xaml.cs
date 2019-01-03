391a40f4331ac96e205e17bdb64d005530da3004
Prasanthi99 <31959931+Prasanthi99@users.noreply.github.com>
2019-01-03 11:49:25 +0530

    Add files via upload

---
+++ MainView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Controls;
+using TaskManager.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TaskManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView :TabbedPage
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            this.Children.Add(new TasksView() { Title = "New", Icon = "new_task.png" });
+            this.Children.Add(new TasksView() { Title = "InProgress", Icon = "in_progress.png" });
+            this.Children.Add(new TasksView() { Title = "Completed", Icon = "completed.png" });
+        }
+
+        protected override void OnCurrentPageChanged()
+        {
+
+            TasksViewModel res = (TasksViewModel)this.CurrentPage.BindingContext;
+            res.PageChanged(this.CurrentPage.Title);
+            //res.BindingContext
+            //res.Title = "Task Manager";
+            base.OnCurrentPageChanged();
+        }
+    }
+}
---
+++ TasksView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TaskManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TasksView : ContentPage
+    {
+        public TasksView()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
